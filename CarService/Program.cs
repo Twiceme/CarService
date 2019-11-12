e2e1471ca67835c56f4d07ec7645041559fc3fa8
Twiceme <bence.kovesi@gmail.com>
2019-11-12 01:49:03 +0100

    csvreader added

--- CarService/Program.cs
+++ CarService/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Text;
+using System.Globalization;
 
 namespace CarService
 {
@@ -14,6 +17,15 @@ namespace CarService
         [STAThread]
         static void Main()
         {
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("hu-HU");
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";      
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            ClintDataCsvReader a = new ClintDataCsvReader();
+            CentralDataCsvReader v = new CentralDataCsvReader();
+            v.ReadDataFromCSV();
+            a.ReadDataFromCSV();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
